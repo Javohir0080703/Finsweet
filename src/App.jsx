0f353e7796a169c46a38c81d9c6523c62675047a
Javohir0080703 <renobobopocta@gmail.com>
2024-01-10 13:39:54 +0500

    I created Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements,} from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Home from './pages/Home'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route  element={<MainLayout/>}>
+        <Route path="/" element={<Home/>} />
+      </Route>
+    )
+   )
+
   return (
-    <div>salom hayot </div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+const MainLayout = () => {
+
+  
+  return (
+    <>
+      <Header/>
+      <main>
+        <Outlet />
+      </main>
+      <Footer/>
+    </>
+  )
+}
+
+export default MainLayout
